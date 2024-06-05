2d59d8f2312affe8733a0229fc6786c6dba5c6ae
Cristian Andres Valenzuela <tc.cvalenzuela@gmail.com>
2024-06-05 13:12:26 -0400

    Actualizacion archivos locales
    Agregar archivos locales de estilos y js

--- limonatura/settings.py
+++ limonatura/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'limonatura.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'b8lb6pubtbzhtffgylo6',
@@ -87,17 +87,17 @@ DATABASES = {
             'autocommit': True,
         }
     }
-}
+} """
 
 
-""" 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
- """
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/core/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
