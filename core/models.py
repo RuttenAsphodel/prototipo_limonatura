ed14fc671a24b9a13adf6eddcc35c9d26b594cc0
Cristian Andres Valenzuela <tc.cvalenzuela@gmail.com>
2024-06-13 21:17:18 -0400

    Update models.py

--- core/models.py
+++ core/models.py
@@ -3,12 +3,11 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Categoria(models.Model):
-    id_category = models.IntegerField(primary_key=True, null=False)
     descripcion_categoria = models.CharField(max_length=255, blank=False)
 
 
 class Producto(models.Model):
-    id_producto = models.IntegerField(primary_key=True, null=False)
+    sku_producto = models.CharField(primary_key=True, max_length=255, blank=False)
     nombre_producto = models.CharField(max_length=255, blank=False)
     descripcion_producto = models.CharField(max_length=255, blank=False)
     precio_producto = models.IntegerField(null=False)
@@ -16,13 +15,13 @@ class Producto(models.Model):
     categoria_id = models.ForeignKey(Categoria, verbose_name=("Categoria"), on_delete=models.CASCADE)
   
 
+class MedioPago(models.Model):
+    descripcion_medio = models.CharField(max_length=55, blank=False)
+    
+
 class Pedido(models.Model):
-    pedido_id = models.IntegerField(primary_key=True, null=False)
     fecha_pedido = models.DateTimeField(auto_now_add=True)
     estado_pedido = models.CharField(max_length=255, choices=[('Pendiente', 'Pendiente'), ('Procesado', 'Procesado'), ('Enviado', 'Entregado'), ('Entregado', 'Entregado')], default='Pendiente')
     usuario_id = models.ForeignKey(User, on_delete=models.CASCADE)
     id_producto = models.ForeignKey(Producto,on_delete=models.CASCADE, verbose_name=("Producto"))
-
-class MedioPago(models.Model):
-    descripcion_medio = models.CharField(max_length=55, blank=False)
-    
+    id_medio = models.ForeignKey(MedioPago, on_delete=models.CASCADE, verbose_name=("Medio de Pago"))
