e5be17c03f683e6c96cd17807f9e1d263fed9b73
Cristian Andres Valenzuela <tc.cvalenzuela@gmail.com>
2024-06-13 21:45:36 -0400

    formuularios

---
+++ core/forms.py
@@ -0,0 +1,39 @@
+from django import forms 
+from models import *
+
+# Forms para Categorias
+class FormCrearCategoria(forms.ModelForm):
+    class Meta:
+        models = Categoria
+        fields = ['descripcion_categoria']
+        widgets = {
+            'Categoria': forms.TextInput(attrs={
+                'placeholder':'Ingrese descripcion'
+                'required'
+                }),
+            }
+        
+        labels = {'descripcion_categoria':'Categoria'}
+        
+
+# Forms para Medios de Pago
+class FormCrearMedioPago(forms.ModelForm):
+    class Meta: 
+        models = MedioPago
+        fields = ['descripcion_medio']
+        widgets = {
+            'MedioPago': forms.TextInput(attrs={
+                'placeholder':'Ingrese medio de pago'
+                'required'}),
+        }
+        
+        labels = {'descripcion_medio', 'Medio de Pago'}
+    
+    # Valida si exite medio de pago 
+    def validarMedioPago(self):
+        mediopago = self.cleaned_data['descripcion_medio']
+        if MedioPago.objects.filter(descripcion_medio = mediopago).exists():
+            raise forms.ValidationError('Medio de Pago ya existe')
+        return mediopago
+    
+    
--- core/views.py
+++ core/views.py
@@ -9,8 +9,37 @@ def home(request):
     return render(request, 'home.html')
 
 # Vistas CRUD Categorias - Alejandra Vidal 
+def listar_categoria(request):
+    return render(request, 'listar_categoria.html')
+
+def detalle_categoria(request):
+    return render(request, 'detalle_categoria.html')
+
+def crear_categoria(request):
+    return render(request, 'crear_categoria.html')
+
+def actualizar_categoria(request):
+    return render(request, 'actualizar_categoria.html')
+
+def eliminar_categoria(request):
+    return render(request, 'detalle_categoria.html')
+
 # Vistas Autenticacion - Alejandra Vidal
 
 
 # Vistas CRUD Productos - Cristian Valenzuela 
 # Vistas CRUD Medios de pagos - Cristian Valenzuela
+
+def listar_medio_pago(request):
+    return render(request, 'listar_medio_pago.html')
+
+def crear_medio_pago(request):
+    return render(request, 'crear_medio_pago.html')
+
+def actualizar_medio_pago(request):
+    return render(request, 'actualizar_medio_pago.html')
+
+def detalle_medio_pago(request):
+    return render(request, 'detalle_medio_pago.html')
+
+
